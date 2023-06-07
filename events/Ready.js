61bf70edc0c6c1c4ef64bae96a2837f69fd5d1f8
Glitch (glitch-hello-node) <none>
2023-06-07 11:03:39 +0000

    📒👯 Checkpoint ./index.js:41556106/277 ./events/Ready.js:41556106/129 ./commands/public/ping.js:34167823/7:41556106/45

--- events/Ready.js
+++ events/Ready.js
@@ -1,7 +1,7 @@
 const { EventBuilder } = require('handler.djs');
 const { Events } = require('discord.js')
 
-const Event = new EventBuilder()
+module.exports = new EventBuilder()
 .setEvent(Events.ClientReady)
 .setExecution(Execute)
 
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const { Application } = require('handler.djs');
+const { Zoblox } = require('zoblox.js');
 const path = require('node:path');
 
 const client = new Client({ intents: [
@@ -9,12 +10,19 @@ const client = new Client({ intents: [
     ] 
 });
 
+const zoblox = new Zoblox()
+
 new Application(client, {
   commandsPath: path.join(__dirname, 'commands'),
+  EventsPath: path.join(__dirname, 'events')
 });
 
 client.Application.setPrefix("!");
 
 client.Application.build();
+
+client.Application.setData({
+  zob: zoblox
+});
 
 client.login(process.env.Token);
