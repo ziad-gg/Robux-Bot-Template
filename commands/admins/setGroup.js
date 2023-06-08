bbdae07457218541a983b5027d598becceb0c068
Glitch (glitch-hello-node) <none>
2023-06-08 10:19:56 +0000

    🐷🦁 Checkpoint ./commands/admins/setGroup.js:34167823/5242 ./index.js:34167823/36 ./commands/public/balance.js:34167823/507 ./commands/public/ping.js:34167823/12 ./commands/public/skin.js:34167823/41

--- commands/admins/setGroup.js
+++ commands/admins/setGroup.js
@@ -2,18 +2,17 @@ const { CommandBuilder } = require('handler.djs');
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = new CommandBuilder() 
-.setName('setgroup')
-.setDescription("Set Main Roblox Group.")
-.setCategory("admins")
-.InteractionOn(new SlashCommandBuilder().addNumberOption(option => option.setName('id').setDescription('Group Id To Select').setRequired(true)))
-.setGlobal(GlobalExecute)
-.setInteractionExecution(InteractionExecute)
-.setMessageExecution(MessageExecute)
+  .setName('setgroup')
+  .setDescription("Set Main Roblox Group.")
+  .setCategory("admins")
+  .InteractionOn(new SlashCommandBuilder().addNumberOption(option => option.setName('id').setDescription('Group Id To Select').setRequired(true)))
+  .setGlobal(GlobalExecute)
+  .setInteractionExecution(InteractionExecute)
+  .setMessageExecution(MessageExecute)
 
 async function GlobalExecute(message, interaction) { 
   const controller =  message ?? interaction;
   const roblox = controller.getData('roblox');
-  
   const GroupId = controller[0];
   
   if (!GroupId) return controller.replyNoMention({ content: "يجب عليك ادخال اي دي الجروب" });
@@ -23,16 +22,15 @@ async function GlobalExecute(message, interaction) {
   if (!Group) return controller.replyNoMention({ content: "يبدو ان اي دي الجروب غير صحيح" });
   
   const owner = await Group.members.get(roblox.me.id);
-  
   if (!owner || !owner.isOwner()) return controller.replyNoMention({})
   
 };
 
 function InteractionExecute(interaction, global) {
-  interaction.replyNoMention({embeds: [global]});
+  interaction.replyNoMention({ embeds: [global] });
 };
 
 function MessageExecute(message, global) {   
-  message.replyNoMention({embeds: [global]});
+  message.replyNoMention({ embeds: [global] });
 };
 
--- commands/public/skin.js
+++ commands/public/skin.js
@@ -2,24 +2,24 @@ const { CommandBuilder } = require('handler.djs');
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = new CommandBuilder() 
-.setName("skin")
-.setDescription("Get your roblox skin.")
-.setCategory("public")
-.setCooldown('10s')
-.InteractionOn(new SlashCommandBuilder().addStringOption(option => option.setName('username').setDescription('please write player username here').setRequired(true)))
-.setGlobal(GlobalExecute)
-.setInteractionExecution(InteractionExecute)
-.setMessageExecution(MessageExecute)
+  .setName("skin")
+  .setDescription("Get your roblox skin.")
+  .setCategory("public")
+  .setCooldown('10s')
+  .InteractionOn(new SlashCommandBuilder().addStringOption(option => option.setName('username').setDescription('please write player username here').setRequired(true)))
+  .setGlobal(GlobalExecute)
+  .setInteractionExecution(InteractionExecute)
+  .setMessageExecution(MessageExecute)
 
 async function GlobalExecute(message, interaction) {
   
   const roblox = message ? message.getData('roblox') : interaction.getData('roblox');
   const username = message ? message[0] : interaction['username'];
-  
-  const m = message ?? interaction;
+  const msg = message ?? interaction;
   
   let user = await roblox.users.find({ userNames: username });
-  if (!user) return message.replyNoMention({content: '**لم يتم العثور على هذا الاسم في روبلوكس، قم بالمحاولة مرة اخرى**'});
+  
+  if (!user) return message.replyNoMention('**لم يتم العثور على هذا الاسم في روبلوكس، قم بالمحاولة مرة اخرى**');
   user = await roblox.users.get(user.id);
 
   const embed = new EmbedBuilder()
@@ -27,7 +27,7 @@ async function GlobalExecute(message, interaction) {
   .setTitle(user.name)
   .setURL('https://www.roblox.com/users/' + user.id)
   .setImage(user.avatarURL())
-  .setFooter({ text: m.author.username, iconURL: m.author.avatarURL() })
+  .setFooter({ text: msg.author.username, iconURL: msg.author.avatarURL() })
   .setTimestamp();
   
   return {
