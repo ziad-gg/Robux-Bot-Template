a1376c48f8610208be55739cf51a17c09d0972ce
Glitch (bot-robux) <none>
2023-06-13 12:46:53 +0000

    👨‍👨‍👦🎣 Checkpoint ./validation/dm.js:34167823/14:41556106/554

--- validation/dm.js
+++ validation/dm.js
@@ -5,11 +5,18 @@ module.exports = new Validation()
 .setCommnads('all')
 .setExecution(Exection)
   
-function Exection (controller, next, end) {
-   if (controller.channel.type === ChannelType.DM && controller.Command.category == 'admins') {
-     controller.reply({ content: "🔴 **Admin Command Is Not Allowed In Direct Channels !**" });
-     return end();
-   } else {
-     next()
-   }
+function Exection(controller, next, end) {
+  const allowed = ['balance', 'transfer']
+  
+  if (controller.channel?.type === ChannelType.DM && controller.Command.category == 'admins') {
+    controller.reply({ content: "🔴 **Admin Command Is Not Allowed In Direct Channels !**" });
+    return end();
+  } else if (controller.channel?.type === ChannelType.DM && !allowed.includes(controller.Command.name)) {
+    controller.reply({ content: "🔴 **Command Is Not Allowed In Direct Channels !**" });
+    return end();
+  } 
+  else {
+    next();
+  };
+  
 }
