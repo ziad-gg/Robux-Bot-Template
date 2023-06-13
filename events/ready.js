602f547846a2cb90c0fa53fed58b25ce881c09b7
Glitch (bot-robux) <none>
2023-06-13 14:29:02 +0000

    🏭🕍 Checkpoint ./events/ready.js:34167823/320 ./src/Constants.js:34167823/190

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 const { EventBuilder } = require('handler.djs');
 const { Events } = require('discord.js');
-const uptime = new (require('uptimer-web').Ugijr
+const uptime = new (require('uptimer-web').UptimeBuilder)({ TYPE: 'Array', URLS: [require('../src/Constants.js').PROJECT_LINK], TIMEOUT: 24e4 });
 const express = require('express');
 const app = express();
 
@@ -11,5 +11,6 @@ module.exports = new EventBuilder()
 async function Execute(client) {
   app.listen(3000);
   app.get('/', (req, res) => res.status(200).send('OK'));
+  uptime.startAll();
   console.log(`${client.user.tag} Is Online !`);
 }
--- src/Constants.js
+++ src/Constants.js
@@ -4,6 +4,8 @@ module.exports.DEFAULT_PREFIX = '-'; //Prefix
 
 module.exports.DEFAULT_GUILD = '936974185421475864'; //Server Id
 
+module.exports.PROJECT_LINK = 'https://bot-robux.glitch.me/'; // Your Project Link
+
 module.exports.CLIENT_OPTIONS = {
   intents: 3276799,
   partials: [Partials.Channel, Partials.User, Partials.Message, Partials.Reaction, Partials.GuildMember],
