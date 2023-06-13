d5c708f113584ae2b136e37c0e19b7f50ecefdbc
Glitch (bot-robux) <none>
2023-06-13 12:24:00 +0000

    ❣️🐯 Checkpoint ./commands/admins/set.js:41556106/297 ./commands/admins/prefix.js:41556106/2298

--- commands/admins/prefix.js
+++ commands/admins/prefix.js
@@ -1,46 +1,35 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const { CommandBuilder } = require('handler.djs');
-const { SlashCommandBuilder } = require('discord.js');
-const { DEFAULT_PREFIX } = require('../../src/Constants.js');
 
-module.exports = new CommandBuilder() 
+module.exports = new CommandBuilder()
 .setName('prefix')
-.setDescription('Change Current Prefix To Another One')
-.InteractionOn(new SlashCommandBuilder().addStringOption((option) => option 
-   .setName('prefix')
-   .setDescription('The New Prefix To Set')
-   .setRequired(false)))
+.setDescription('delete a User in the database')
+.InteractionOn(new SlashCommandBuilder())
 .setGlobal(Global)
-.setInteractionExecution(InteractionExecute)
-.setMessageExecution(InteractionExecute)
-.isSubCommand()
+.setMessageExecution(Message)
+.setInteractionExecution(Interaction)
+.isSubCommand();
 
-async function Global(message, interaction, global) {
-  const controller = message ?? interaction;
-  const guild = global.guild;
-  
-  console.log("Hello World");
-  
-  const prefix = controller[0];
-    
-  if (!prefix) {
-    guild.prefix = prefix;
-    await guild.save();
-  } else {
-    guild.prefix = DEFAULT_PREFIX;
-    await guild.save();
-  };
- 
-  controller.replyNoMention({ content: '✅' });
+async function Global (message, interaction, global) {
 
-  
-   return {
+  console.log(global)
+
+  const controller = message ?? interaction
+
+  await controller.reply({ content: "Hello World From Global Global Response" })
+
+  return {
     interaction: true,
-    message: true
+    message: "man"
   }
-  
 }
 
-async function InteractionExecute(interaction, global) {
-  console.log("Hello World From Interaction")
-  
+function Message (message, global) {
+  console.log(global)
+  message.reply({ content: "Hello World From Child Message Response" })
+}
+
+async function Interaction (interaction, global) {
+  console.log(global)
+  interaction.followUp({ content: "Hello World From Child Interaction Response" })
 }
--- commands/admins/set.js
+++ commands/admins/set.js
@@ -8,25 +8,24 @@ module.exports = new CommandBuilder()
   .setGlobal(GlobalExecute)
   .OwnersOnly()
   .setSubcommands([
-     // { command: 'prefix' },     
-     { command: 'delete' },
-     // { command: 'group' },
-     // { command: 'clientsrole' },
-     // { command: 'donechannel' },
-     // { command: 'max', group: 'buy' },
-     // { command: 'min', group: 'buy' },
-     // { command: 'max', group: 'transfer' },
-     // { command: 'min', group: 'transfer' },
+     { command: 'prefix' },     
+     // { command: 'delete' },
+     { command: 'group' },
+     { command: 'clientsrole' },
+     { command: 'donechannel' },
+     { command: 'max', group: 'buy' },
+     { command: 'min', group: 'buy' },
+     { command: 'max', group: 'transfer' },
+     { command: 'min', group: 'transfer' },
   ])
 
 async function GlobalExecute(message, interaction) {
-  const controller = message ?? interaction;
-  const Guilds = controller.getData('guilds');
-  const guild = await Guilds.get(controller.guild.id);
+//   const controller = message ?? interaction;
+//   const Guilds = controller.getData('guilds');
+//   const guild = await Guilds.get(controller.guild.id);
   
-  return {
-    interaction: {
-      guild
-    }
+   return {
+    interaction: "Hello World From Parent To Interaction",
+    message: "Hello World From Parent To Message"
   }
 }
