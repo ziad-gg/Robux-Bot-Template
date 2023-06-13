f9be7942a3839613a881e7e7dcb7de18bb183eea
Glitch (bot-robux) <none>
2023-06-13 12:58:11 +0000

    🍪⛽️ Checkpoint ./validation/dm.js:34167823/1407

--- validation/dm.js
+++ validation/dm.js
@@ -2,21 +2,15 @@ const { Validation } = require('handler.djs');
 const { ChannelType } = require('discord.js');
 
 module.exports = new Validation()
-.setCommnads('all')
-.setExecution(Exection)
-  
+  .setCommnads('all')
+  .setExecution(Exection);
+
 function Exection(controller, next, end) {
-  const allowed = ['balance', 'transfer']
-  
-  if (controller.channel?.type === ChannelType.DM && controller.Command.category == 'admins') {
-    controller.reply({ content: "🔴 **Admin Command Is Not Allowed In Direct Channels !**" });
+  const allowed = ['balance', 'transfer'];
+
+  if (controller.channel?.type === ChannelType.DM && !allowed.includes(controller.Command.name)) {
     return end();
-  } else if (controller.channel?.type === ChannelType.DM && !allowed.includes(controller.Command.name)) {
-    controller.reply({ content: "🔴 **Command Is Not Allowed In Direct Channels !**" });
-    return end();
-  } 
-  else {
+  } else {
     next();
-  };
-  
+  }
 }
