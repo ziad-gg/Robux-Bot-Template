2e6886cdffdc3d685d53823631edf5066fe6199c
Glitch (bot-robux) <none>
2023-06-19 12:17:27 +0000

    📟🌟 Checkpoint ./server/views/dashboard.ejs:41556106/71428 ./server/views/home.ejs:34167823/5:41556106/15474 ./server/routes/home.js:41556106/26

--- server/routes/home.js
+++ server/routes/home.js
@@ -6,7 +6,8 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.render('home', {
     title: client.user.username + " | " + "dashboard",
-    client
+    client,
+    user: req.user
   });
 })
 
