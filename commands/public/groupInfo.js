5e3c1195fac5a96e9cbb001bb4c299c007bf270b
Glitch (bot-robux) <none>
2023-06-21 11:02:38 +0000

    ⭐️🍁 Checkpoint ./commands/public/groupInfo.js:34167823/1352

--- commands/public/groupInfo.js
+++ commands/public/groupInfo.js
@@ -14,8 +14,18 @@ async function GlobalExecute(message, interaction) {
     const guildData = await controller.getData('guilds').get(controller.guild.id);
     const group = await controller.getData('roblox').groups.get(guildData.groupId);
     const embed = new EmbedBuilder()
-     .setColor('#0be881')
-    
+      .setColor('#0be881')
+      .setAuthor({ name: group.name, iconURL: group.logoURL() })
+      .setTitle(group.name)
+      .setURL(group.linkURL())
+      .setThumbnail(group.logoURL())
+      .addFields([{ name: '🆔 Group ID', value: `${group.id}` }])
+      .addFields([{ name: '📅 Created On', value: `<t:${Math.floor(+new Date(group.shout.created) / 1000)}:R>` }])
+      .addFields([{ name: '👥 Total Members', value: `${group.memberCount}` }])
+      .addFields([{ name: '🤴 Group Owner', value: `${group.owner.username} (${group.owner.userId})` }])
+      .addFields([{ name: '🕒 Pending Robux', value: `${await group.fetchRevenueSummary().then((e) => e.pendingRobux)}` }])
+      .addFields([{ name: '💎 Total Robux', value: `${await group.fetchCurrency().then((e) => e.robux)}` }])
+      .addFields([{ name: '🔺 Cashed Out', value: `${await group.fetchRevenueSummary().then((e) => e.groupPayoutRobux.toString().replace('-', ''))}` }]);
     controller.replyNoMention({ embeds: [embed] });
   } catch {
     controller.replyNoMention({ content: '❌ **حدث خطأ ما**' });
