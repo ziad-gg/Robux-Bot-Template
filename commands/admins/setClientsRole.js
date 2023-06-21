e9eeacaf170473bbceff9e0947cb8afd7019593a
Glitch (bot-robux) <none>
2023-06-21 11:25:44 +0000

    🐦🏰 Checkpoint ./commands/admins/setClientsRole.js:34167823/279

--- commands/admins/setClientsRole.js
+++ commands/admins/setClientsRole.js
@@ -17,15 +17,15 @@ async function GlobalExecute(message, interaction, global) {
   
   if (!controller[0]) return controller.replyNoMention({ content: '❌ **قم بتحديد الرتبة!**' });
   
-  const role = controller.guild.roles.cache.get(interaction ? interaction[0].id : controller[0]);
+  const role = controller.guild.roles.cache.get(interaction ? interaction[0].id : controller[0].toId());
 
   console.log(role);
   
-  if (!role || role.name === '@everyone' || role.name === '@here') return controller.replyNoMention({ content: '❌ **قم بتحديد رتبة صالحة!**' });
+  if (!role || role.tags.botId) return controller.replyNoMention({ content: '❌ **قم بتحديد رتبة صالحة!**' });
   if (guildData.clientsRole === role.id) return controller.replyNoMention({ content: '❌ **هذه الرتبة محددة من قبل!**' });
   
   guildData.clientsRole = role.id;
   await guildData.save();
   
-  interaction.replyNoMention({ content: '✅ **تم بنجاح تحديد رتبة العملاء!**' });
+  controller.replyNoMention({ content: '✅ **تم بنجاح تحديد رتبة العملاء!**' });
 };
