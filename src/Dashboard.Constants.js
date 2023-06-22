e9952478d4a36010e15dcf164a8fbfe46921f730
Glitch (bot-robux) <none>
2023-06-22 22:39:16 +0000

    🐁🚪 Checkpoint ./src/Dashboard.Constants.js:41556106/3758 ./site/views/home.ejs:41556106/22524

--- src/Dashboard.Constants.js
+++ src/Dashboard.Constants.js
@@ -1,17 +1,53 @@
 module.exports = {
   home: {
-    p1: 'A multifunctional bot for helping Robux servers!',
-    p2: 'Robux, Welcomer, Embed Messages, Logs, and more with an easy Dashboard!',
-    SupportButton: {
-      url: '',
-      text: 'Support'
+    Header: {
+      home: {
+        text: 'Home',
+        url: '/',
+        disabled: false,
+      },
+      commands: {
+        text: 'Commands',
+        url: '/',
+        disabled: true,
+      },
+      tranfer: {
+        text: 'Tranfer',
+        url: '/dashboard#balance',
+        disabled: false,
+      },
+      support: {
+        text: 'Support',
+        url: '',
+        disabled: false,
+      },
     },
-    LoginButton: {
-      url: '/login',
-      text: 'Login'
+    body: {
+      p1: 'A multifunctional bot for helping Robux servers!',
+      p2: 'Robux, Welcomer, Embed Messages, Logs, and more with an easy Dashboard!',
+      SupportButton: {
+        style: 'secondary',
+        url: '',
+        text: 'Support'
+      },
+      DashboardButton: {
+        style: 'primary',
+        url: '/dashboard',
+        text: 'Dashboard'
+      },
+      LoginButton: {
+        style: 'primary',
+        url: '/login',
+        text: 'Login'
+      },
+      LogoutButton: {
+        style: 'danger',
+        url: '/logout',
+        text: 'Logout'
+      },
+      LeftImg: {
+        url: 'https://cdn.discordapp.com/attachments/1116475279012003931/1120062108873281676/robux.png'
+      }, 
     }
   },
-  dashboard: {
-    
-  }
 }
