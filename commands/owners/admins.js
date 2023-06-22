786c86fcc114bbccf150f5eb8fbc4b123b47f39c
Glitch (bot-robux) <none>
2023-06-22 16:27:40 +0000

    🏖🏖 Checkpoint ./commands/owners/remove.js:41556106/5931 ./commands/owners/admins.js:41556106/57 ./package.json:41556106/107

--- commands/owners/admins.js
+++ commands/owners/admins.js
@@ -11,6 +11,7 @@ module.exports = new CommandBuilder()
   .OwnersOnly()
   .setSubcommands([
      { command: 'add', group: 'admins' },
+     { command: 'remove', group: 'admins' },
   ]);
 
 async function GlobalExecute(message, interaction) {
