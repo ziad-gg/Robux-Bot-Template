0630d1ae405357812b8a40be2e08691e84bd346b
Glitch (bot-robux) <none>
2023-06-23 22:16:33 +0000

    🌋🌆 Checkpoint ./commands/public/timer.js:34167823/2989 ./package.json:34167823/1458

--- commands/public/timer.js
+++ commands/public/timer.js
@@ -1,5 +1,6 @@
 const { CommandBuilder } = require('handler.djs');
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const humanizeDuration = require('humanize-duration');
 
 module.exports = new CommandBuilder() 
   .setName('timer')
@@ -9,7 +10,7 @@ module.exports = new CommandBuilder()
   .setCooldown('10s')
   .InteractionOn(new SlashCommandBuilder().setDMPermission(false).addStringOption((option) => option
      .setName('username')
-     .setDescription('The username to get skin for')                                                            
+     .setDescription('The name you want to find for the 14-day entry date')                                                            
      .setRequired(true)))
   .setGlobal(GlobalExecute)
   .setAttr('args', 1)
@@ -18,22 +19,22 @@ module.exports = new CommandBuilder()
 async function GlobalExecute(message, interaction) {
   const controller = message ?? interaction;
   try {
-    const roblox = message ? message.getData('roblox') : interaction.getData('roblox');
-    const username = controller[0];
-  
-    let user = await roblox.users.find({ userNames: username });
-  
+    const requestsData = controller.getData('requests');
+    const guildsData = controller.getData('guilds');
+    const roblox = controller.getDate('roblox');
+    
+    let user = await roblox.users.find({ userIds: controller[0] });
     if (!user) return controller.replyNoMention({ content: '❌ **يبدو أن هذا اللاعب غير متواجد في روبلوكس!**' });
+    
     user = await roblox.users.get(user.id);
-
-    const embed = new EmbedBuilder()
-      .setAuthor({ name: user.name, iconURL: user.avatarURL() })
-      .setTitle(user.name)
-      .setURL(user.profileURL)
-      .setImage(user.avatarURL())
-      .setFooter({ text: controller.author.username, iconURL: controller.author.avatarURL() })
-      .setTimestamp();
-  
+    const guildData = await guildData.get(controller.guild.id);
+    const requestData = await requestsData.findOne({ groupId: guildData.group, userId: user.id });
+    const isCompleted = !requestData ? true : Date.now() + 1209600000 <= requestData.joinDate ? true : false;
+    if (isCompleted) return controller.replyNoMention({ content: '🥳 **هذا المستخدم لقد اكمل 14 يوم بالفعل في الجروب!**' });
+    
+    const unix = Math.floor(+new Date(Request.joinDate) / 1000 + 1209600);
+    controller.replyNoMention({ content: `**${humanizeDuration(+new Date(Request.joinDate) + 1209600000 - Date.now(), { language: 'ar', round: true })} وتكمل اسبوعين في الجروب**`, new MessageEmbed().setColor('GREEN').setAuthor(User2.name , User2.avatarURL({type:'headshot'}) , `https://web.roblox.com/users/${User2.id}/profile`).setTitle(`ستكمل خلال:\n<t:${unix}:R>,<t:${unix}:F>\nدخلت الجروب منذ:\n<t:${unix2}:R>,<t:${unix2}:F>`).setFooter(message.author.tag , message.author.avatarURL({dynamic:true}))
+    
     controller.replyNoMention({ embeds: [embed] });
   } catch {
     return controller.replyNoMention({ content: '❌ **حدث خطأ ما**' });
