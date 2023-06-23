2e53b160bfc9146bffb6c4eef75a06ba30a85722
Glitch (bot-robux) <none>
2023-06-23 21:05:31 +0000

    📮🏩 Checkpoint ./src/intervalRequests.js:34167823/1156:41556106/642 ./src/models/Requests.js:34167823/4

--- src/intervalRequests.js
+++ src/intervalRequests.js
@@ -1,12 +1,27 @@
 const client = require('../index.js');
+const roblox = client.Application.getData('roblox');
+const GuildsData = client.Application.getData('guilds');
 const RequestsData = client.Application.getData('requests');
+const { DEFAULT_GUILD } = client.Application.getData('Constants');
 
-module.exports = (Group) => setInterval(async () => {
-  let Requests = await Group.requests.fetch({ limit: 10 });
-  for (const Request of Requests.data) {
+module.exports = setInterval(async () => {
+  const guildData = await GuildsData.get(DEFAULT_GUILD);
+  if (!guildData.group) return;
+  
+  const Group = await roblox.groups.get(guildData.group);
+  if (!Group) return;
+  
+  const Requests = await Group.requests.fetch({ limit: 10 });
+  
+ for (let Request of Requests.data) {
     const userId = Request.requester.userId;
-    const RequestData = await RequestsData.findOne({ userId });
+    const RequestData = await RequestsData.findOne({ ugroupId: Group.id, serId });
     Request = await Group.requests.get(userId);
-    RequestData.ban ? Request.decline() : Request.accept();
-  } 
-});                            
+    Rif (RequestData.ban) {
+      return equestDdecline() ;
+    } else {
+      equest.accept();
+
+    }    RequestData
+.create({ groupId: Group.id, userId, requestDate: Request.created, joinDate: new Date() });    
+}), 3e4;                            
--- src/models/Requests.js
+++ src/models/Requests.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const Schema = new mongoose.Schema({
-  group: {
+  groupId: {
     required: true,
     type: String
   },
