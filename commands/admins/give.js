73d14a5f4cf12fd7e663d25b35fc28ad926d9809
Glitch (bot-robux) <none>
2023-07-01 18:36:36 +0000

    Exporting to SlayverB/robux-bot ./commands/admins/remove.js:34167823/225 ./commands/admins/give.js:34167823/219

--- commands/admins/give.js
+++ commands/admins/give.js
@@ -26,7 +26,10 @@ async function GlobalExecute(message, interaction) {
   if (!amount.isNumber()) return controller.replyNoMention({ content: '❌ **يجب أن تقوم بتحديد رقم صحيح!**' });
   
   const user = await controller.getUser(userId).then(u => u?.user?.id? u.user : u);
-  const userData = await usersData.get(user.id, controller.guild.id);
+  if (!user) return controller.replyNoMention({ content: '❌ **لا يمكنني العثور على هذا العضو!**' });
+  if (user.bot) return controller.replyNoMention({ content: '❌ **البوتات لا تملك حساب!**' });
+  
+  const userData = await usersData.get(user.id);
   
   userData.balance += amount;
   await userData.save();
