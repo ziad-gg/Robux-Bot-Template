279daaac77affea7d82389540dc0049367039a96
Glitch (bot-robux) <none>
2023-07-13 14:15:43 +0000

    🏆🎆 Checkpoint ./index.js:34167823/1 ./src/intervalRequests.js:34167823/179

--- src/intervalRequests.js
+++ src/intervalRequests.js
@@ -5,31 +5,35 @@ const RequestsData = client.Application.getData('requests');
 const { DEFAULT_GUILD } = client.Application.getData('Constants');
 
 async function setup() {
-  const guildData = await GuildsData.get(DEFAULT_GUILD);
-  if (!guildData.group) return;
+  try {
+    const guildData = await GuildsData.get(DEFAULT_GUILD);
+    if (!guildData.group) return;
 
-  const Group = await roblox.groups.get(guildData.group);
-  if (!Group) return;
+    const Group = await roblox.groups.get(guildData.group);
+    if (!Group) return;
 
-  setInterval(async () => {
-    const Requests = await Group.requests.fetch();
+    setInterval(async () => {
+      const Requests = await Group.requests.fetch();
 
-    for (let Request of Requests.data) {
-      const userId = Request.requester.userId;
-      const RequestData = await RequestsData.findOne({
-        groupId: Group.id,
-        userId
-      });
-      Request = await Group.requests.get(userId);
-      Request.accept();
-      RequestsData.createNewRequest({
-        groupId: Group.id,
-        userId,
-        requestDate: Request.created,
-        joinDate: new Date()
-      });
-    }
+      for (let Request of Requests.data) {
+        const userId = Request.requester.userId;
+        const RequestData = await RequestsData.findOne({
+          groupId: Group.id,
+          userId
+        });
+        Request = await Group.requests.get(userId);
+        Request.accept();
+        RequestsData.createNewRequest({
+          groupId: Group.id,
+          userId,
+          requestDate: Request.created,
+          joinDate: new Date()
+       });
+     }
   }, 3e4);
+  } catch {
+    return;
+  };
 };
 
 module.exports = setup;
